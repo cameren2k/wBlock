8ff04dcae45b4e9f4d60bb4799f81a83e51959bc
0xCUB3 <skula@mit.edu>
2026-04-20 18:15:10 -0400

    reset cask to 2.0.1

--- Casks/wblock.rb
+++ Casks/wblock.rb
@@ -1,6 +1,6 @@
 cask "wblock" do
-  version "2.0.2"
-  sha256 "7a67045111ff1ba2daa61e3f3f8bc627e1ede90bce8e28eb0e1d6a82cb46315a"
+  version "2.0.1"
+  sha256 "059874d72a162ce54593cbe84aee5934e503505e47dd7b02849d6cb94001de92"
 
   url "https://github.com/0xCUB3/wBlock/releases/download/#{version}/wBlock-#{version}.dmg",
       verified: "github.com/0xCUB3/wBlock/"
